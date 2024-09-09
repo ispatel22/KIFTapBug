1b7d2616de26ddb8e76a0833b0304091f4136aaa
Ishan <ispatel@ptc.com>
2024-09-09 13:43:47 -0400

    Added Pods folder to git ignore

--- Pods/Target Support Files/KIF-Core-IdentifierTests/KIF-Core-IdentifierTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_KIF_Core_IdentifierTests : NSObject
-@end
-@implementation PodsDummy_KIF_Core_IdentifierTests
-@end
--- Pods/Target Support Files/Pods-KIFTapBug/Pods-KIFTapBug-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KIFTapBug : NSObject
-@end
-@implementation PodsDummy_Pods_KIFTapBug
-@end
